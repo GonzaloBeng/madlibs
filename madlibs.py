89234dc998553ccafb0ff3dedf6d40fbbfb0dab5
Gonzalo Beng <gonzalobeng@gmail.com>
2021-11-07 20:41:50 -0300

    Madlibs

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+
+""" Concatenar cadenas de caracteres """
+
+# Mad Libs
+
+adj = input('Adjetivo: ')
+verbo_1 = input('Verbo: ')
+verbo_2 = input('Verbo: ')
+sustantivo_plural = input('Sustantivo (plural): ')
+
+madlib = f'Programar es tan {adj}!. Siempre me emociona porque me encanta {verbo_1} problemas. Aprende a {verbo_2} para alcanzar tus {sustantivo_plural}!.'
+
+
+print(madlib)
